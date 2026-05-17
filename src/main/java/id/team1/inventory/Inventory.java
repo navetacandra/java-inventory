fa4cb87f6ea8bf9c225849362a6a671e41f84507
navetacandra <dev@navetacandraa.my.id>
2026-05-17 11:27:05 +0700

    chore: project init

---
+++ src/main/java/id/team1/inventory/Inventory.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package id.team1.inventory;
+
+/**
+ *
+ * @author team1
+ */
+public class Inventory {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
